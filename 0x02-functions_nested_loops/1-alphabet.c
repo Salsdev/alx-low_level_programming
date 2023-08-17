d1748256b0b07bbdaf14fea63b45e46f5fd65f8c
salsdev <salehaliyu111@gmail.com>
2023-08-16 17:07:10 -0700

    print lower case alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - starting point
+ *
+ * Description: Alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (char i = 'a'; i < 'z'; i++)
+	{
+		putchar("%c\n", char);
+	}
+	return (0);
+}
